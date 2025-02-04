a48c544627d66c785f8f4f79cf8052359b0530f4
yokoshima228 <sakashimaideal@yandex.ru>
2025-02-04 19:35:51 +0800

    added config to git

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string     `yaml:"env"`
+	StoragePath string     `yaml:"storage_path"`
+	HttpServer  HttpServer `yaml:"http_server"`
+}
+
+type HttpServer struct {
+	Address     string        `yaml:"address"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+	User        string        `yaml:"user"`
+	Password    string        `yaml:"password"`
+}
+
+func MustLoad() *Config {
+	configPath := flag.String("config", "./config/local.yaml", "path to config file")
+	flag.Parse()
+
+	if _, err := os.Stat(*configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file is not exists %s", *configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(*configPath, &cfg); err != nil {
+		log.Fatalf("Error reading config %s\n", err.Error())
+	}
+
+	return &cfg
+}
